605b1744f86b89e6d404dbb2eb400f763466529d
“Aleksey <aleksey.kudryavtsew@gmail.com>
2025-09-07 16:51:06 +0300

    exception+english

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Calculator {
     public static void main(String[] args) {
@@ -6,18 +7,23 @@ public class Calculator {
         Scanner in = new Scanner(System.in);
         String text = """
                 Выберите действие:
-                0: Выйти
-                1: Сложениие
-                2: Вычитание
-                3: Умножение
-                4: Деление
-                5: Факториал
+                0: Exit
+                1: Addition
+                2: Subtraction
+                3: Multiplication
+                4: Division
+                5: Factorial
                 """;
         System.out.println(text);
+        int i = 0;
+        try {
+            i = in.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("Error: the entered symbol does not meet the condition.");
+            main(null);
+        }
 
-        int i = in.nextInt();
-
-        switch (i){
+        switch (i) {
             case 0:
                 return;
             case 1:
@@ -41,80 +47,86 @@ public class Calculator {
                 main(null);
                 break;
             default:
-                System.out.println("Операция не найдена");
+                System.out.println("Operation not found");
+                main(null);
         }
     }
 
-    static void printSum(){
-        double x,y;
+    static void printSum() {
+        double x, y;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое слагаемое:");
+        System.out.println("Enter the first term:");
         x = in.nextDouble();
-        System.out.println("Введите второе слагаемое:");
+        System.out.println("Enter the second term:");
         y = in.nextDouble();
-        System.out.println("Сумма:\n"+sum(x,y));
+        System.out.println("The sum:\n" + sum(x, y));
     }
-    static double sum(double x, double y){
+
+    static double sum(double x, double y) {
         return x + y;
     }
 
-    static void printDif(){
-        double x,y;
+    static void printDif() {
+        double x, y;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите уменьшаемое:");
+        System.out.println("Enter a diminutive:");
         x = in.nextDouble();
-        System.out.println("Введите вычитаемое:");
+        System.out.println("Enter the deductible:");
         y = in.nextDouble();
-        System.out.println("Разность:\n"+dif(x,y));
+        System.out.println("The difference:\n" + dif(x, y));
     }
-    static double dif(double x, double y){
+
+    static double dif(double x, double y) {
         return x - y;
     }
 
-    static void printMul(){
-        double x,y;
+    static void printMul() {
+        double x, y;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первый множитель:");
+        System.out.println("Enter the first multiplier:");
         x = in.nextDouble();
-        System.out.println("Введите второй множитель:");
+        System.out.println("Enter the second multiplier:");
         y = in.nextDouble();
-        System.out.println("Произведение:\n"+mul(x,y));
-    }
-    static double mul(double x, double y){
-        return x*y;
+        System.out.println("The product:\n" + mul(x, y));
     }
 
-    static void printDiv(){
-        double x,y;
+    static double mul(double x, double y) {
+        return x * y;
+    }
+
+    static void printDiv() {
+        double x, y;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите делимое:");
+        System.out.println("Enter the divisible:");
         x = in.nextDouble();
-        System.out.println("Введите делитель:");
+        System.out.println("Enter the divisor:");
         y = in.nextDouble();
-        try{
-            System.out.println("Частное:\n"+div(x,y));
-        }
-        catch (ArithmeticException ex){
+        try {
+            System.out.println("The quotient:\n" + div(x, y));
+        } catch (ArithmeticException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
     }
-    static double div(double x, double y){
-        if(y == 0){
-            throw  new ArithmeticException("Dividing into zero");
-        }
-        return x/y; }
 
-    static void printFact(){
+    static double div(double x, double y) {
+        if (y == 0) {
+            throw new ArithmeticException("Dividing into zero");
+        }
+        return x / y;
+    }
+
+    static void printFact() {
         int x;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите целое число X, не превышающее 20:");
+        System.out.println("Enter an integer X not exceeding 20:");
         x = in.nextInt();
-        System.out.println("X! = "+fact(x));
+        System.out.println("X! = " + fact(x));
     }
-    static long fact(int x){
-        if(x==1){
+
+    static long fact(int x) {
+        if (x == 1) {
             return 1;
         }
-        return x*fact(x-1);
+        return x * fact(x - 1);
     }
 }
